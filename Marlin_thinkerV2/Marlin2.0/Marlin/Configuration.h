4f380a878e8f24fdd65748a02017bd891b16c5dd
Christian Ashby <git@cashby.me.uk>
2020-05-09 18:54:19 +0100

    Z_SAFE_HOMING for centre of bed

--- Marlin_thinkerV2/Marlin2.0/Marlin/Configuration.h
+++ Marlin_thinkerV2/Marlin2.0/Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "CAThinkerV2 v0.2"
+#define CUSTOM_MACHINE_NAME "CAThinkerV2 v0.3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
@@ -933,7 +933,7 @@
 #define NOZZLE_TO_PROBE_OFFSET { -39, -11, 0 }
 
 // Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 10
+#define MIN_PROBE_EDGE 30
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
@@ -1339,7 +1339,7 @@
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing when homing all axes (G28).
 // - Prevent Z homing when the Z probe is outside bed area.
 //
-//#define Z_SAFE_HOMING
+#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT ((X_BED_SIZE) / 2)    // X point for Z homing when homing all axes (G28).
