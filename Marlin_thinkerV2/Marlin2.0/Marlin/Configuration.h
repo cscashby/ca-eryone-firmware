d6d4a8f5653e9c48b93852f34306a12d08093df3
Christian Ashby <me@cashby.me.uk>
2021-09-26 16:15:00 +0200

    First add of dan's v2

--- Marlin_thinkerV2/Marlin2.0/Marlin/Configuration.h
+++ Marlin_thinkerV2/Marlin2.0/Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Dan's Thinker V2-BL (0.7)"
+#define CUSTOM_MACHINE_NAME "Dan's Thinker2 V2-BL (0.1)"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
